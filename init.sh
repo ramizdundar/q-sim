42a10af1786d84062ec13c5bf6dd95fa92610bf5
ramizdundar <ramizdundar@gmail.com>
2021-09-03 14:39:41 +0300

    finalize init.sh

--- init.sh
+++ init.sh
@@ -4,19 +4,43 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo $SCRIPT_DIR
 
 cd $SCRIPT_DIR/unisocket-clients
-echo $SCRIPT_DIR/unisocket-clients
 rm -f out-clients.txt
 
 cd $SCRIPT_DIR/test-latency-before
-echo $SCRIPT_DIR/test-latency-before
 rm -f out-test.txt
 
-#
+cd $SCRIPT_DIR/test-latency-josef
+rm -f out-test.txt
+
+cd $SCRIPT_DIR/test-latency-jozsi
+rm -f out-test.txt
+
+cd $SCRIPT_DIR/test-throughput-before
+rm -f out-test.txt
+
+cd $SCRIPT_DIR/test-throughput-josef
+rm -f out-test.txt
+
+cd $SCRIPT_DIR/test-throughput-jozsi
+rm -f out-test.txt
+
+################################
 cd $SCRIPT_DIR/unisocket-clients
 provisioner --kill >> out-clients.txt 2>&1
-./run 0 3000 20m >> out-clients.txt 2>&1 &
+./run 0 3000 15m >> out-clients.txt 2>&1 &
  
 cd $SCRIPT_DIR/test-latency-before
+provisioner --kill >> out-test.txt 2>&1
+./run 3 8 20m >> out-test.txt 2>&1 &
+
+wait
+
+################################
+cd $SCRIPT_DIR/unisocket-clients
+provisioner --kill >> out-clients.txt 2>&1
+./run 0 3000 15m >> out-clients.txt 2>&1 &
+ 
+cd $SCRIPT_DIR/test-throughput-jozsi
 provisioner --kill >> out-test.txt 2>&1
 ./run 3 8 20m >> out-test.txt 2>&1 &
 
