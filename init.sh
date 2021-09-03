bb97da3b079841fea85b23a0089090ac00543438
ramizdundar <ramizdundar@gmail.com>
2021-09-03 14:40:56 +0300

    finalize init.sh

--- init.sh
+++ init.sh
@@ -24,7 +24,7 @@ rm -f out-test.txt
 cd $SCRIPT_DIR/test-throughput-jozsi
 rm -f out-test.txt
 
-################################
+# Latency - Before
 cd $SCRIPT_DIR/unisocket-clients
 provisioner --kill >> out-clients.txt 2>&1
 ./run 0 3000 15m >> out-clients.txt 2>&1 &
@@ -35,7 +35,51 @@ provisioner --kill >> out-test.txt 2>&1
 
 wait
 
-################################
+# Latency - Josef
+cd $SCRIPT_DIR/unisocket-clients
+provisioner --kill >> out-clients.txt 2>&1
+./run 0 3000 15m >> out-clients.txt 2>&1 &
+ 
+cd $SCRIPT_DIR/test-latency-josef
+provisioner --kill >> out-test.txt 2>&1
+./run 3 8 20m >> out-test.txt 2>&1 &
+
+wait
+
+# Latency - Jozsi
+cd $SCRIPT_DIR/unisocket-clients
+provisioner --kill >> out-clients.txt 2>&1
+./run 0 3000 15m >> out-clients.txt 2>&1 &
+ 
+cd $SCRIPT_DIR/test-latency-jozsi
+provisioner --kill >> out-test.txt 2>&1
+./run 3 8 20m >> out-test.txt 2>&1 &
+
+wait
+
+# Throughput - Before
+cd $SCRIPT_DIR/unisocket-clients
+provisioner --kill >> out-clients.txt 2>&1
+./run 0 3000 15m >> out-clients.txt 2>&1 &
+ 
+cd $SCRIPT_DIR/test-throughput-before
+provisioner --kill >> out-test.txt 2>&1
+./run 3 8 20m >> out-test.txt 2>&1 &
+
+wait
+
+# Throughput - Josef
+cd $SCRIPT_DIR/unisocket-clients
+provisioner --kill >> out-clients.txt 2>&1
+./run 0 3000 15m >> out-clients.txt 2>&1 &
+ 
+cd $SCRIPT_DIR/test-throughput-josef
+provisioner --kill >> out-test.txt 2>&1
+./run 3 8 20m >> out-test.txt 2>&1 &
+
+wait
+
+# Throughput - Jozsi
 cd $SCRIPT_DIR/unisocket-clients
 provisioner --kill >> out-clients.txt 2>&1
 ./run 0 3000 15m >> out-clients.txt 2>&1 &
