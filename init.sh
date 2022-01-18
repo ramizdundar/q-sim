7fe048320c8307625aaa646ea8c017c4d1f926f3
Ufuk Yılmaz <ufuk.yilmaz@hazelcast.com>
2022-01-18 17:04:32 +0300

    Update the init.sh to print info about the running tests

--- init.sh
+++ init.sh
@@ -6,6 +6,8 @@ echo "This script does not call \`provisioner --install\` since it is not needed
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo $SCRIPT_DIR
 
+# Cleanup the outputs of previous runs
+echo Starting to cleanup
 cd $SCRIPT_DIR/unisocket-clients
 rm -f out-clients.txt
 
@@ -27,68 +29,84 @@ rm -f out-test.txt
 cd $SCRIPT_DIR/test-throughput-with-hostnames-5.1-BETA-1
 rm -f out-test.txt
 
+echo Finished cleanup
+
 # Latency - Before
+echo Running the latency test with the commit before the hostname fix
 cd $SCRIPT_DIR/unisocket-clients
 provisioner --kill >> out-clients.txt 2>&1
-./run 0 3000 15m >> out-clients.txt 2>&1 &
+./run 0 250 15m >> out-clients.txt 2>&1 &
  
 cd $SCRIPT_DIR/test-latency-before
 provisioner --kill >> out-test.txt 2>&1
 ./run 2 4 20m >> out-test.txt 2>&1 &
 
 wait
+echo Finished the latency test with the commit before the hostname fix
 
 # Latency - 5.1-BETA-1
+echo Running the latency test with 5.1-BETA-1
 cd $SCRIPT_DIR/unisocket-clients
 provisioner --kill >> out-clients.txt 2>&1
-./run 0 3000 15m >> out-clients.txt 2>&1 &
+./run 0 250 15m >> out-clients.txt 2>&1 &
  
 cd $SCRIPT_DIR/test-latency-5.1-BETA-1
 provisioner --kill >> out-test.txt 2>&1
 ./run 2 4 20m >> out-test.txt 2>&1 &
 
 wait
+echo Finished the latency test with 5.1-BETA-1
 
 # Latency - 5.1-BETA-1 with hostnames
+echo Running the latency test with hostnames on 5.1-BETA-1
 cd $SCRIPT_DIR/unisocket-clients
 provisioner --kill >> out-clients.txt 2>&1
-./run 0 3000 15m >> out-clients.txt 2>&1 &
+./run 0 250 15m >> out-clients.txt 2>&1 &
  
 cd $SCRIPT_DIR/test-latency-with-hostnames-5.1-BETA-1
 provisioner --kill >> out-test.txt 2>&1
 ./run 2 4 20m >> out-test.txt 2>&1 &
 
 wait
+echo Finished the latency test with hostnames on 5.1-BETA-1
 
 # Throughput - Before
+echo Running the throughput test with the commit before the hostname fix
 cd $SCRIPT_DIR/unisocket-clients
 provisioner --kill >> out-clients.txt 2>&1
-./run 0 3000 15m >> out-clients.txt 2>&1 &
+./run 0 250 15m >> out-clients.txt 2>&1 &
  
 cd $SCRIPT_DIR/test-throughput-before
 provisioner --kill >> out-test.txt 2>&1
 ./run 2 4 20m >> out-test.txt 2>&1 &
 
 wait
+echo Finished the throughput test with the commit before the hostname fix
+
 
 # Throughput - 5.1-BETA-1
+echo Running the throughput test with 5.1-BETA-1
 cd $SCRIPT_DIR/unisocket-clients
 provisioner --kill >> out-clients.txt 2>&1
-./run 0 3000 15m >> out-clients.txt 2>&1 &
+./run 0 250 15m >> out-clients.txt 2>&1 &
  
 cd $SCRIPT_DIR/test-throughput-5.1-BETA-1
 provisioner --kill >> out-test.txt 2>&1
 ./run 2 4 20m >> out-test.txt 2>&1 &
 
 wait
+echo Finished the throughput test with 5.1-BETA-1
+
 
 # Throughput - 5.1-BETA-1 with hostnames
+echo Running the throughput test with hostnames on 5.1-BETA-1
 cd $SCRIPT_DIR/unisocket-clients
 provisioner --kill >> out-clients.txt 2>&1
-./run 0 3000 15m >> out-clients.txt 2>&1 &
+./run 0 250 15m >> out-clients.txt 2>&1 &
  
 cd $SCRIPT_DIR/test-throughput-with-hostnames-5.1-BETA-1
 provisioner --kill >> out-test.txt 2>&1
 ./run 2 4 20m >> out-test.txt 2>&1 &
 
 wait
+echo Finished the throughput test with hostnames on 5.1-BETA-1
