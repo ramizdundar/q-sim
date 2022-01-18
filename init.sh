52220cac283b9cb605a00fa104b055b98331d1c6
Ufuk Yılmaz <ufuk.yilmaz@hazelcast.com>
2022-01-18 14:39:42 +0300

    Prepare run with 5.1-BETA-1

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+echo "This script does not call \`provisioner --install\` since it is not needed for
+  each run. Make sure you installed the simulator on agent machines before
+  running this script."
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo $SCRIPT_DIR
@@ -9,19 +12,19 @@ rm -f out-clients.txt
 cd $SCRIPT_DIR/test-latency-before
 rm -f out-test.txt
 
-cd $SCRIPT_DIR/test-latency-josef
+cd $SCRIPT_DIR/test-latency-5.1-BETA-1
 rm -f out-test.txt
 
-cd $SCRIPT_DIR/test-latency-jozsi
+cd $SCRIPT_DIR/test-latency-with-hostnames-5.1-BETA-1
 rm -f out-test.txt
 
 cd $SCRIPT_DIR/test-throughput-before
 rm -f out-test.txt
 
-cd $SCRIPT_DIR/test-throughput-josef
+cd $SCRIPT_DIR/test-throughput-5.1-BETA-1
 rm -f out-test.txt
 
-cd $SCRIPT_DIR/test-throughput-jozsi
+cd $SCRIPT_DIR/test-throughput-with-hostnames-5.1-BETA-1
 rm -f out-test.txt
 
 # Latency - Before
@@ -31,29 +34,29 @@ provisioner --kill >> out-clients.txt 2>&1
  
 cd $SCRIPT_DIR/test-latency-before
 provisioner --kill >> out-test.txt 2>&1
-./run 3 8 20m >> out-test.txt 2>&1 &
+./run 2 4 20m >> out-test.txt 2>&1 &
 
 wait
 
-# Latency - Josef
+# Latency - 5.1-BETA-1
 cd $SCRIPT_DIR/unisocket-clients
 provisioner --kill >> out-clients.txt 2>&1
 ./run 0 3000 15m >> out-clients.txt 2>&1 &
  
-cd $SCRIPT_DIR/test-latency-josef
+cd $SCRIPT_DIR/test-latency-5.1-BETA-1
 provisioner --kill >> out-test.txt 2>&1
-./run 3 8 20m >> out-test.txt 2>&1 &
+./run 2 4 20m >> out-test.txt 2>&1 &
 
 wait
 
-# Latency - Jozsi
+# Latency - 5.1-BETA-1 with hostnames
 cd $SCRIPT_DIR/unisocket-clients
 provisioner --kill >> out-clients.txt 2>&1
 ./run 0 3000 15m >> out-clients.txt 2>&1 &
  
-cd $SCRIPT_DIR/test-latency-jozsi
+cd $SCRIPT_DIR/test-latency-with-hostnames-5.1-BETA-1
 provisioner --kill >> out-test.txt 2>&1
-./run 3 8 20m >> out-test.txt 2>&1 &
+./run 2 4 20m >> out-test.txt 2>&1 &
 
 wait
 
@@ -64,28 +67,28 @@ provisioner --kill >> out-clients.txt 2>&1
  
 cd $SCRIPT_DIR/test-throughput-before
 provisioner --kill >> out-test.txt 2>&1
-./run 3 8 20m >> out-test.txt 2>&1 &
+./run 2 4 20m >> out-test.txt 2>&1 &
 
 wait
 
-# Throughput - Josef
+# Throughput - 5.1-BETA-1
 cd $SCRIPT_DIR/unisocket-clients
 provisioner --kill >> out-clients.txt 2>&1
 ./run 0 3000 15m >> out-clients.txt 2>&1 &
  
-cd $SCRIPT_DIR/test-throughput-josef
+cd $SCRIPT_DIR/test-throughput-5.1-BETA-1
 provisioner --kill >> out-test.txt 2>&1
-./run 3 8 20m >> out-test.txt 2>&1 &
+./run 2 4 20m >> out-test.txt 2>&1 &
 
 wait
 
-# Throughput - Jozsi
+# Throughput - 5.1-BETA-1 with hostnames
 cd $SCRIPT_DIR/unisocket-clients
 provisioner --kill >> out-clients.txt 2>&1
 ./run 0 3000 15m >> out-clients.txt 2>&1 &
  
-cd $SCRIPT_DIR/test-throughput-jozsi
+cd $SCRIPT_DIR/test-throughput-with-hostnames-5.1-BETA-1
 provisioner --kill >> out-test.txt 2>&1
-./run 3 8 20m >> out-test.txt 2>&1 &
+./run 2 4 20m >> out-test.txt 2>&1 &
 
 wait
